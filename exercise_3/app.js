3648a02b95aa52068bac2fd9e00398be5e55d6bf
James Billard <James.Billard@indigoslate.com>
2018-10-08 13:41:56 -0700

    used computed

--- exercise_3/app.js
+++ exercise_3/app.js
@@ -3,6 +3,11 @@ new Vue({
 	data: {
 		value: 0
 	},
+	computed: {
+		result: function() {
+			return this.value < 37 ? 'not there yet' : 'done'
+		}
+	},
 	watch: {
 		value: function() {
 			let vi = this
