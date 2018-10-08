71ea140be171d8e2a0635d8289d3cf44aab52a22
James Billard <James.Billard@indigoslate.com>
2018-10-08 12:43:16 -0700

    exercise complete

--- exercise_2/app.js
+++ exercise_2/app.js
@@ -1,6 +1,17 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+	el: '#exercise',
+	data: {
+		value: ''
+	},
+	methods: {
+		alertFn: function() {
+			alert('THIS IS AN ALERT')
+		},
+		storeValue: function(e) {
+			this.value = e.target.value
+		},
+		storeValueOnEnter: function(e) {
+			this.value = e.target.value
+		}
+	}
+})
